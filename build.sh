33112704de3b3c619f389b684ea62e1ad5206506
Henrique N. Lengler <henriqueleng@openmailbox.org>
2015-12-06 21:46:42 -0300

    beter css file, propper title id

--- build.sh
+++ build.sh
@@ -23,8 +23,8 @@ cat <<!__EOF__
   <body>
   <div id="wrapper">
   <header>
-    <h1><a href=$3>$TITLE</a></h1>
-	<h2>&mdash; $SUBTITLE</h2>
+    <h1 id="title"><a href=$3>$TITLE</a></h1>
+	<h2 id="subtitle>&mdash; $SUBTITLE</h2>
   </header>
 <li id="navbar"><a href=$3>home</a></li>
 !__EOF__
